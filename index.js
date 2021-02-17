589004a99e22ed85459962e862535e7ff995692b
Joe Hildebrand <joe-github@cursive.net>
2021-02-17 10:26:30 -0700

    add ava, es2020 env

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 module.exports = {
   env: {
     node: true,
-    es6: true
+    es6: true,
+    es2020: true
   },
   plugins: [
-    'node'
+    'node',
+    'ava'
   ],
   parserOptions: {
     ecmaVersion: 2020
@@ -190,10 +192,34 @@ module.exports = {
     'node/no-unsupported-features/es-syntax': 'error',
     'node/shebang': 'warn',
     'node/no-deprecated-api': 'error',
-    'node/prefer-global/url': 'error'
+    'node/prefer-global/url': 'error',
 
     // One day?
     // 'no-magic-numbers': 'warn',
     // 'node/no-unsupported-features/node-builtins': 'error'
+
+    // ava
+    'ava/assertion-arguments': 'error',
+    'ava/no-async-fn-without-await': 'error',
+    'ava/no-duplicate-modifiers': 'error',
+    'ava/no-identical-title': 'error',
+    'ava/no-ignored-test-files': 'error',
+    'ava/no-incorrect-deep-equal': 'error',
+    'ava/no-invalid-end': 'error',
+    'ava/no-nested-tests': 'error',
+    'ava/no-only-test': 'error',
+    'ava/no-skip-assert': 'error',
+    'ava/no-skip-test': 'error',
+    'ava/no-statement-after-end': 'error',
+    'ava/no-todo-test': 'error',
+    'ava/no-unknown-modifiers': 'error',
+    'ava/prefer-t-regex': 'error',
+    'ava/test-ended': 'error',
+    'ava/test-title-format': 'error',
+    'ava/test-title': 'error',
+    'ava/use-t-throws-async-well': 'error',
+    'ava/use-t-well': 'error',
+    'ava/use-t': 'error',
+    'ava/use-test': 'error'
   }
 }
